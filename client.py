281775d168bc85afc9f4d212c6f2db9d01c49cb1
Maiaroca <146460693+Maiaroca@users.noreply.github.com>
2025-03-20 11:52:02 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,29 +1,27 @@
 from paillier import Paillier
 
 class Client:
-    def __init__(self, bits=1024):
-        print("Generating client key pair...")
-        self.paillier = Paillier(bits)
+    def __init__(self, bits=1024): 
+        self.paillier = Paillier(bits) # constructor genereted the paillier cryptosystem
         self.public_key = self.paillier.public_key
         self.private_key = self.paillier.private_key
-        print("Client key pair generated!")
 
-    def create_request(self, db_size, index):
-        """ Creates a PIR request by encrypting a selection vector """
+    def request(self, db_size, index):
+        """ Creates a PIR request by encrypting a selection output """
         print(f"Creating request for index {index} in a database of size {db_size}")
-        request_vector = []
+        request_output = []
 
         for i in range(db_size):
             if i == index:
                 encrypted_value = self.paillier.encrypt(1)
             else:
                 encrypted_value = self.paillier.encrypt(0)
-            request_vector.append(encrypted_value)
+            request_output.append(encrypted_value)
 
-        print("Request vector created!")
-        return request_vector
+        print("Request output created")
+        return request_output
 
-    def decrypt_response(self, encrypted_response):
+    def decryptAnswer(self, encrypted_response):
         """ Decrypts the server's response to retrieve the requested value """
         print("Decrypting server response...")
         result = self.paillier.decrypt(encrypted_response)
