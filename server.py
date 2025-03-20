69013eb477885d68919369f58208cf2ff26df96b
Maiaroca <146460693+Maiaroca@users.noreply.github.com>
2025-03-20 11:54:04 +0100

    Update server.py

--- server.py
+++ server.py
@@ -5,19 +5,20 @@ class Server:
     def __init__(self, db_size=10):
         print(f"Generating a database with {db_size} elements...")
         self.db_size = db_size
-        self.database = [random.randint(1, 100) for _ in range(db_size)]
+        self.database = [random.randint(1, 2**16) for _ in range(db_size)]
         print(f"Database initialized: {self.database}")
 
-    def process_request(self, request_vector, public_key):
+    def answerRequest(self, request_vector, public_key):
         """ Computes the homomorphic response based on the request vector """
-        print("Processing PIR request...")
-        encrypted_sum = 1  # Start with neutral element for multiplication
+        encrypted_sum = 1 #Start
+        operations_count = 0  # Counter for multiplications
 
         for i in range(self.db_size):
             encrypted_value = request_vector[i]
             encrypted_sum *= pow(encrypted_value, self.database[i], public_key[0]**2)  # Homomorphic multiplication
             encrypted_sum %= public_key[0]**2  # Keep result within n²
+            operations_count += 1  # Counting operations
 
-        print("Computation complete! Sending encrypted response...")
+        print(f"Computation Server complete. Number of multiplications: {operations_count}")
         return encrypted_sum
 
